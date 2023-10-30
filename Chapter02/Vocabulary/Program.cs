7683b7ed3997215717036789bab949af5cf2db49
Jonathan Vos <vosjonjoe@gmail.com>
2023-10-29 18:32:25 -0700

    Started Chapter 2

---
+++ Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+System.Data.DataSet ds;
+HttpClient client;
+
+Assembly? myApp = Assembly.GetEntryAssembly();
+if (myApp == null) return;
+
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    Assembly a = Assembly.Load(name);
+
+    int methodCount = 0;
+
+    foreach(TypeInfo t in a.DefinedTypes)
+    {
+        methodCount += t.GetMethods().Count();
+    }
+
+    Console.WriteLine(
+        "{0:N0} types with {1:N0} methods in {2} assembly",
+        arg0: a.DefinedTypes.Count(),
+        arg1: methodCount,
+        arg2: name.Name);
+}
