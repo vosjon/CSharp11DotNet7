9c9038b4490bfd78c764736145539a9e8eb17580
Jonathan Vos <vosjonjoe@gmail.com>
2023-11-09 18:34:13 -0800

    Completed the AsyncConsole project for chapter 2.

---
+++ Chapter02/AsyncConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿HttpClient client = new();
+HttpResponseMessage response =
+    await client.GetAsync("http://www.netproinnovations.com/");
+WriteLine("NETPro Innovations home page has {0:N0} bytes.",
+    response.Content.Headers.ContentLength);
+
